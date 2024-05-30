3a23dd4ef2c10611fd50bde03130ce7b2c3beeb9
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-30 19:22:30 +0300

    feat: writing class instance methods and attributest

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,9 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Some knowledge", "Some other knowledge"]
+
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
